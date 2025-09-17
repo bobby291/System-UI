78e5ffeaa901b07a392c2811d255692238471f04
Bob Paschal <bobpaschal42gmail.com>
2025-09-17 19:30:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import About from "./components/Hero"
+import Client from "./components/Client"
+import Hero from "./components/About"
+import Offer from "./components/Offer"
+import Portfolio from "./components/Portfolio"
+import Review from "./components/Review"
+import Cta from "./components/cta"
+import Header from "./components/Header"
+
+
+export default function App() {
+  return (
+    <>
+    <Header />
+    <Hero />
+    <About />
+    <Client />
+    <Hero />
+    <Offer />
+    <Portfolio />
+    <Review />
+    <Cta />
+    
+    
+    </>
+  )
+}
